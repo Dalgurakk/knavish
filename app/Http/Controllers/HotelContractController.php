ef9da694b56ade15ea7fc8ded76a7f9ebd0bd746
dalgurakk <delvalmorell8606@gmail.com>
2019-02-15 20:34:29 -0500

    Hotel Contract/Seting: fix search range

--- app/Http/Controllers/HotelContractController.php
+++ app/Http/Controllers/HotelContractController.php
@@ -1220,10 +1220,12 @@ class HotelContractController extends Controller
                     $startRange = Carbon::createFromFormat('!Y-m-d', $range->from);
                     $endRange = Carbon::createFromFormat('!Y-m-d', $range->to);
                     if ($start->greaterThanOrEqualTo($startRange)) {
-                        $startRange = $start;
+                        //$startRange = $start;
+                        $startRange = Carbon::createFromFormat('!Y-m-d', $start->format('Y-m-d'));
                     }
                     if ($end->lessThanOrEqualTo($endRange)) {
-                        $endRange = $end;
+                        //$endRange = $end;
+                        $endtRange = Carbon::createFromFormat('!Y-m-d', $end->format('Y-m-d'));
                     }
                     for ($o = $startRange; $o->lessThanOrEqualTo($endRange); $o->addDay()) {
                         foreach ($offer->rooms as $room) {
